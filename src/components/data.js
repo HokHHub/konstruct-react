de4aee5ab156ee2e53f6c7a7c7f7a18dbc507fd1
Vladimir <adda37@yandex.ru>
2025-04-01 21:00:07 +0300

    добавил базу данных

---
+++ src/components/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        img: './Card/audit.png',
+        title: 'PROJECT AND CONSTRUCTION MANAGEMENT',
+        descr: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.',
+    },
+    {
+        img: './Card/store.png',
+        title: 'STRUCTURE AUDIT AND MAINTENANCE',
+        descr: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.',
+    },
+    {
+        img: './Card/bank.png',
+        title: 'FACTORY CONSTRUCTION AND MODELING',
+        descr: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.',
+    }
+]
