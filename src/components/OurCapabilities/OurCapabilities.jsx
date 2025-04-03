d8fa1da94c2ff0c8fc8b99f90a92b0c3258cb2fc
Timur <abkeevtimur@gmail.com>
2025-04-03 19:38:10 +0300

    ourCapabilities

---
+++ src/components/OurCapabilities/OurCapabilities.jsx
@@ -0,0 +1,27 @@
+import s from './OurCapabilities.module.css'
+import Card from '../Card/Card'
+import {data} from '../data'
+
+export default function OurCapabilities(){
+    return(
+        <>
+            <section className={s.OurCapabilities}>
+                <div className={s.container}>
+                    <div className={s.title}>
+                        <div className={s.title__name}>
+                            <h2 className={s.title__name_h2}>OUR CAPABILITIES</h2>
+                            <span className={s.title__name_span}></span>
+                        </div>
+                        <p className={s.title__about}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+                        <button className={s.title__button}>ALL SERVICES</button>
+                    </div>
+                    <div className={s.articles}>
+                        {data.map((el, i)=>(
+                            <Card img={el.img} title={el.title} descr={el.descr} key={i}/>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
