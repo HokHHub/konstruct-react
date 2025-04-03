1695c80aeb545fac03418388d1afd603c12b4cd8
Vladimir <adda37@yandex.ru>
2025-04-03 21:07:01 +0300

    добавлен хедер

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Story from './components/Story/Story'
 import OurCapabilities from './components/OurCapabilities/OurCapabilities'
 import Container from './components/Container/Container'
+import Header from './components/Header/Header'
+import headerStyle from './components/Header/Header.module.css'
 
 function App() {
 
   return (
     <>
+      <Container>
+        <Header />
+      </Container>
+        <hr className={headerStyle.header__hr} />
       <Container>
         <Story />
       </Container>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
 import s from './Header.module.css'
-import Logo from ''
+import Logo from './headerLogo.svg'
 
 function Header() {
 
-  return (
-    <header className={s.header}>
-        <img src="" alt="" />
-    </header>
-  )
+    return (
+        <>
+            <header className={s.header}>
+                <img className={s.header__img} src={Logo} alt="" />
+                <ul className={s.header__ul}>
+                    <li className={s.header__li}>HOME</li>
+                    <li className={s.header__li}>ABOUT</li>
+                    <li className={s.header__li}>SERVICES</li>
+                    <li className={s.header__li}>WORK</li>
+                    <li className={s.header__li}>CONTACT</li>
+                </ul>
+            </header>
+        </>
+    )
 }
 
 export default Header
