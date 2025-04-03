618a7b3cf39c3dd1df7f15bc4fc2aea12a271669
Vladimir <adda37@yandex.ru>
2025-04-03 19:42:59 +0300

    добавлен файл шрифта

---
+++ src/components/Story/Story.jsx
@@ -0,0 +1,13 @@
+import s from './App.css'
+
+function Story() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <section className={s.story}>
+      
+    </section>
+  )
+}
+
+export default App
